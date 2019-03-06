d41f0fc84efc5184f49a70804abef1f8a9c6396e
Julius Zerwick <jrzerwick@gmail.com>
2019-03-06 18:33:50 -0500

    Split more logic from is_match_helper method into several other methods

--- regex_searcher.rb
+++ regex_searcher.rb
@@ -7,17 +7,10 @@ end
 def is_match_helper(pattern, string, pattern_idx, string_idx)
 	# Base case of reaching the end of both the pattern and string without any false returns
 	if (string_idx >= string.length)
-
 		if (pattern_idx >= pattern.length)
 			return true
 		else
-			if (pattern_idx < pattern.length) && (pattern[pattern_idx - 1] == '*' || pattern[pattern_idx - 1] == '+')
-				return is_match_helper(pattern, string, pattern_idx + 1, string_idx)
-			elsif (pattern_idx < pattern.length) && (pattern[pattern_idx] == '?' || pattern[pattern_idx - 1] == '?')
-				return is_match_helper(pattern, string, pattern_idx + 1, string_idx)
-			else
-				return false
-			end
+			consider_rest_of_pattern(pattern, string, pattern_idx, string_idx)
 		end
 
 	# If we've reached the end of our pattern but not our string, then we do not have a match and return false
@@ -27,17 +20,15 @@ def is_match_helper(pattern, string, pattern_idx, string_idx)
 	# If we haven't reached the end of our pattern or string, and the current pattern char is '*', then we recurse
 	elsif (pattern[pattern_idx] == '*')
 		return is_match_helper(pattern, string, pattern_idx + 1, string_idx)
+
 	elsif (pattern[pattern_idx - 1] == '*')
 		handle_asterisk_or_plus_as_previous_char(pattern, string, pattern_idx, string_idx)
 
 	# Logic for '+'
 
 	elsif (pattern[pattern_idx] == '+')
-		if (pattern[pattern_idx + 1] == '.' || pattern[pattern_idx + 1] == string[string_idx])
-			return is_match_helper(pattern, string, pattern_idx + 1, string_idx)
-		else
-			return false
-		end
+		handle_plus_as_current_char(pattern, string, pattern_idx, string_idx)
+
 	elsif (pattern[pattern_idx - 1] == '+')
 		handle_asterisk_or_plus_as_previous_char(pattern, string, pattern_idx, string_idx)
 
@@ -45,37 +36,60 @@ def is_match_helper(pattern, string, pattern_idx, string_idx)
 
 	elsif (pattern[pattern_idx] == '?')
 		return (is_match_helper(pattern, string, pattern_idx + 1, string_idx) || is_match_helper(pattern, string, pattern_idx + 2, string_idx))
+
 	elsif (pattern[pattern_idx - 1] == '?')
 		handle_question_mark_as_previous_char(pattern, string, pattern_idx, string_idx)
 
 	# Base case where the current char in pattern is '.' or the same as the current char in string. If true, recurse.
-	elsif pattern_char_exact_match?(pattern[pattern_idx], string[string_idx])
+	elsif pattern_and_string_chars_exact_match?(pattern[pattern_idx], string[string_idx])
 		return is_match_helper(pattern, string, pattern_idx + 1, string_idx + 1)
+		
 	else
 		return false
 	end
 end
 
-def pattern_char_exact_match?(pattern_char, string_char)
+
+def consider_rest_of_pattern(pattern, string, pattern_idx, string_idx)
+	if (pattern_idx < pattern.length) && (pattern[pattern_idx - 1] == '*' || pattern[pattern_idx - 1] == '+')
+		return is_match_helper(pattern, string, pattern_idx + 1, string_idx)
+	elsif (pattern_idx < pattern.length) && (pattern[pattern_idx] == '?' || pattern[pattern_idx - 1] == '?')
+		return is_match_helper(pattern, string, pattern_idx + 1, string_idx)
+	else
+		return false
+	end
+end
+
+
+def pattern_and_string_chars_exact_match?(pattern_char, string_char)
 	pattern_char == '.' || (pattern_char == string_char)
 end
 
+
 def handle_asterisk_or_plus_as_previous_char(pattern, string, pattern_idx, string_idx)
-	if pattern_char_exact_match?(pattern[pattern_idx], string[string_idx])
+	if pattern_and_string_chars_exact_match?(pattern[pattern_idx], string[string_idx])
 		return is_match_helper(pattern, string, pattern_idx, string_idx + 1)
 	else
 		return is_match_helper(pattern, string, pattern_idx + 1, string_idx)
 	end
 end
 
+
 def handle_question_mark_as_previous_char(pattern, string, pattern_idx, string_idx)
-	if pattern_char_exact_match?(pattern[pattern_idx], string[string_idx])
+	if pattern_and_string_chars_exact_match?(pattern[pattern_idx], string[string_idx])
 		return is_match_helper(pattern, string, pattern_idx + 1, string_idx + 1)
 	else
 		return is_match_helper(pattern, string, pattern_idx + 1, string_idx)
 	end
 end
 
+def handle_plus_as_current_char(pattern, string, pattern_idx, string_idx)
+	if (pattern[pattern_idx + 1] == '.' || pattern[pattern_idx + 1] == string[string_idx])
+		return is_match_helper(pattern, string, pattern_idx + 1, string_idx)
+	else
+		return false
+	end
+end
 
 
 ################################################
